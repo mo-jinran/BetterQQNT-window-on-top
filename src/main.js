4a1649d69a5bcc216a10f813fd88a446aa28b329
沫烬染 <mo_jinran@outlook.com>
2024-09-27 21:32:12 +0800

    优化代码

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 const { BrowserWindow, ipcMain } = require("electron");
 
 
-// 置顶按钮
-ipcMain.on("LiteLoader.window_on_top.toggleWindowOnTop", (event) => {
+ipcMain.on("mojinran.window_on_top.toggleWindowOnTop", (event) => {
     const window = BrowserWindow.fromWebContents(event.sender);
     window.setAlwaysOnTop(!window.isAlwaysOnTop());
     event.returnValue = window.isAlwaysOnTop();
